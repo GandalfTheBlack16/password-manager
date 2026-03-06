8cfe6caec1d3f111bcfc1999357b43624f07361d
Gabriel Gutierrez <gabriel.gutierrez@gft.com>
2026-03-06 08:59:39 +0100

    Fixed react-router error

--- src/components/vaults/VaultItem.tsx
+++ src/components/vaults/VaultItem.tsx
@@ -1,114 +1,109 @@
-import { useState } from 'react'
-import { FiCopy, FiEye, FiEyeOff, FiEdit3, FiDelete } from 'react-icons/fi' 
-import { useNavigate } from 'react-router'
-import { deleteCredential } from '../../services/VaultService'
-import { useVaultStore } from '../../hooks/stores/useVaultStore'
-import { useToast } from '../../hooks/useToast'
+import { useState } from "react";
+import { FiCopy, FiEye, FiEyeOff, FiEdit3, FiDelete } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { deleteCredential } from "../../services/VaultService";
+import { useVaultStore } from "../../hooks/stores/useVaultStore";
+import { useToast } from "../../hooks/useToast";
 
 type Props = {
-    id?: string,
-    vaultId: string,
-    name: string,
-    description: string,
-    secret: string
-}
+  id?: string;
+  vaultId: string;
+  name: string;
+  description: string;
+  secret: string;
+};
 
-export function VaultItem ({ id, vaultId, name, description, secret }: Props) {
+export function VaultItem({ id, vaultId, name, description, secret }: Props) {
+  const [show, setShow] = useState<boolean>(false);
 
-    const [show, setShow] = useState<boolean>(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const { updateVault } = useVaultStore();
 
-    const { updateVault } = useVaultStore()
+  const { setConfirmMessage, setSuccess } = useToast();
 
-    const { setConfirmMessage, setSuccess } = useToast()
+  const handleShowButtonClick = () => {
+    setShow((curr) => !curr);
+  };
 
-    const handleShowButtonClick = () => {
-        setShow(curr => !curr)
+  const handleCopyButtonClick = () => {
+    navigator.clipboard.writeText(secret);
+    setSuccess("Secret copied to clipboard");
+  };
+
+  const handleEditButtonClick = () => {
+    navigate(`credentials`, {
+      state: {
+        vaultId,
+        credential: { id, name, description, secret },
+      },
+    });
+  };
+
+  const handleDeleteButtonClick = () => {
+    if (id) {
+      setConfirmMessage(`Removing ${name} credential. Are you sure?`, () => {
+        deleteCredential(vaultId, id)
+          .then((vault) => {
+            updateVault(vault);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     }
+  };
 
-    const handleCopyButtonClick = () => {
-        navigator.clipboard.writeText(secret)
-        setSuccess('Secret copied to clipboard')
-    }
-
-    const handleEditButtonClick = () => {
-        navigate(`credentials`, {
-            state: {
-                vaultId,
-                credential: { id, name, description, secret }
-            }
-        })
-    }
-
-    const handleDeleteButtonClick = () => {
-        if (id) {
-            setConfirmMessage(`Removing ${name} credential. Are you sure?`, () => {
-                deleteCredential(vaultId, id)
-                .then(vault => {
-                    updateVault(vault)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-            })
-        }
-    }
-
-    return (
-        <article className="item">
-            <div className="item_header">
-                <div className="item_header-info">
-                    <h3>{name}</h3>
-                    <span>{description}</span>
-                </div>
-                <div className='item_header-actions'>
-                    <button
-                        id={'editCred_' + id}
-                        onClick={handleEditButtonClick}
-                        className='tooltip'
-                    >
-                        <FiEdit3 size={'15px'} />
-                        <span className='tooltip_text'>
-                            Edit credential
-                        </span>
-                    </button>
-                    <button
-                        id={'deleteCred_' + id}
-                        onClick={handleDeleteButtonClick}
-                        className='tooltip'
-                    >
-                        <FiDelete size={'15px'} />
-                        <span className='tooltip_text'>
-                            Delete credential
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <div className="item_action">
-                <input 
-                    id={id} 
-                    type={!show ? "password" : "text"} 
-                    value={secret} 
-                    readOnly 
-                />
-                <button
-                    id={'copyCred_' + id}
-                    className='tooltip'
-                    onClick={handleCopyButtonClick}
-                >
-                    <FiCopy size={'15px'} />
-                    <span className='tooltip_text'>Add to clipboad</span>
-                </button>
-                <button
-                    id={'showCred_' + id}
-                    className={show ? 'toogle tooltip': 'tooltip'}
-                    onClick={handleShowButtonClick}
-                >
-                    {!show ? <FiEye size={'15px'} /> : <FiEyeOff size={'15px'} />}
-                    <span className='tooltip_text'>Show credential</span>
-                </button>
-            </div>
-        </article>
-    )
+  return (
+    <article className="item">
+      <div className="item_header">
+        <div className="item_header-info">
+          <h3>{name}</h3>
+          <span>{description}</span>
+        </div>
+        <div className="item_header-actions">
+          <button
+            id={"editCred_" + id}
+            onClick={handleEditButtonClick}
+            className="tooltip"
+          >
+            <FiEdit3 size={"15px"} />
+            <span className="tooltip_text">Edit credential</span>
+          </button>
+          <button
+            id={"deleteCred_" + id}
+            onClick={handleDeleteButtonClick}
+            className="tooltip"
+          >
+            <FiDelete size={"15px"} />
+            <span className="tooltip_text">Delete credential</span>
+          </button>
+        </div>
+      </div>
+      <div className="item_action">
+        <input
+          id={id}
+          type={!show ? "password" : "text"}
+          value={secret}
+          readOnly
+        />
+        <button
+          id={"copyCred_" + id}
+          className="tooltip"
+          onClick={handleCopyButtonClick}
+        >
+          <FiCopy size={"15px"} />
+          <span className="tooltip_text">Add to clipboad</span>
+        </button>
+        <button
+          id={"showCred_" + id}
+          className={show ? "toogle tooltip" : "tooltip"}
+          onClick={handleShowButtonClick}
+        >
+          {!show ? <FiEye size={"15px"} /> : <FiEyeOff size={"15px"} />}
+          <span className="tooltip_text">Show credential</span>
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/layouts/AuthPageLayout.tsx
+++ src/layouts/AuthPageLayout.tsx
@@ -1,22 +1,18 @@
-import { Navigate } from "react-router";
+import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../hooks/stores/useAuthStore";
 import { Header } from "../components/header/Header";
 import { Toaster } from "react-hot-toast";
 
-export function AuthPageLayout ({ children }: { children: JSX.Element }) {
-    const { isLogged } = useAuthStore()
-    if (!isLogged) {
-        return <Navigate to={'/login'} replace/>
-    }
-    return (
-        <>
-            <Toaster 
-                position="bottom-center"
-            />
-            <Header />
-            <main>
-                { children }
-            </main>
-        </>
-    )
+export function AuthPageLayout({ children }: { children: JSX.Element }) {
+  const { isLogged } = useAuthStore();
+  if (!isLogged) {
+    return <Navigate to={"/login"} replace />;
+  }
+  return (
+    <>
+      <Toaster position="bottom-center" />
+      <Header />
+      <main>{children}</main>
+    </>
+  );
 }
